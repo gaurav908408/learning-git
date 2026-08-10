bb706fe12035c01a3015db8de079bf92adb32292
Gaurav Kaushik <gauravkaushik882@gmail.com>
2026-08-10 13:56:24 +0530

    Add update todo API

--- backend/Routes/todoRoutes.js
+++ backend/Routes/todoRoutes.js
@@ -5,20 +5,18 @@ const router = express.Router();
 const {
     addTodo,
     getTodos,
-    deleteTodo
+    deleteTodo,
+    updateTodo
 } = require("../Controllers/todoController");
 
 
-// ADD TODO
 router.post("/todos", addTodo);
 
-
-// GET ALL TODOS
 router.get("/todos", getTodos);
 
-
-// DELETE TODO
 router.delete("/todos/:id", deleteTodo);
+
+router.put("/todos/:id", updateTodo);
 
 
 module.exports = router;
