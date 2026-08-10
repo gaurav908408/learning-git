2351415f2144f647cda16f6b600857b499ed05e1
Gaurav Kaushik <gauravkaushik882@gmail.com>
2026-08-10 13:50:33 +0530

    Add delete todo API

--- backend/Routes/todoRoutes.js
+++ backend/Routes/todoRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
+
 const router = express.Router();
 
 const {
     addTodo,
-    getTodos
+    getTodos,
+    deleteTodo
 } = require("../Controllers/todoController");
 
 
@@ -13,6 +15,10 @@ router.post("/todos", addTodo);
 
 // GET ALL TODOS
 router.get("/todos", getTodos);
+
+
+// DELETE TODO
+router.delete("/todos/:id", deleteTodo);
 
 
 module.exports = router;
